5f4bb99a91ebdfaefaca50799fcac0dd1fd7e516
Rohit Mandava <rohitmandava@Rohits-MacBook-Pro.local>
2017-07-26 10:14:22 -0500

    Initial Commit

---
+++ autolayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  autolayout
+//
+//  Created by Rohit Mandava on 7/26/17.
+//  Copyright © 2017 Rohit Mandava. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
